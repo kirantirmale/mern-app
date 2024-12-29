431959dc9d7d98f896ed5dff230a55ce1a175a0c
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-12-29 12:34:55 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ function Signup() {
         }
 
         try {
-            const url = 'http://localhost:8080/auth/signup';
+            const url = 'https://mern-app-api-xi.vercel.app/auth/signup';
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
