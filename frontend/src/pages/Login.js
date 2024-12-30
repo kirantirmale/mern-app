e07b32c6f28f76da0bab9d6430e315c720a5c54d
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-12-30 17:46:07 +0530

    css ply

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -66,7 +66,7 @@
         
 
     return (
-        <section className="bg-teal-500  ">
+        <section className="bg-teal-500 h-screen w-full flex items-center justify-center">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
